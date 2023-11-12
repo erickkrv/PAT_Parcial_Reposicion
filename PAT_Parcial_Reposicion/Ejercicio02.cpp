7041f04b17cf8437c904b812ce1f5ff903bb9a66
erickkrv <rivaal04@gmail.com>
2023-11-12 11:32:22 -0600

    ej1 y 2

--- PAT_Parcial_Reposicion/Ejercicio02.cpp
+++ PAT_Parcial_Reposicion/Ejercicio02.cpp
@@ -1,6 +1,40 @@
 #include "Ejercicio02.h"
 
+Node<char>* Ejercicio02::reverse(Node<char>* prev, Node<char>* next) {
+	Node<char>* ultimo = prev->next;
+	Node<char>* actual = ultimo->next;
+	while (actual != next) {
+		ultimo->next = actual->next;
+		actual->next = prev->next;
+		prev->next = actual;
+		actual = ultimo->next;
+	}
+	return ultimo;
+}
 Node<char>* Ejercicio02::reverseKGroup(Node<char>* head, int k)
 {
-    return nullptr;
+	if (head == nullptr) {
+		return nullptr;
+	}
+	else if (k == 1) {
+		return head;
+	}
+	else {
+		Node<char>* dummy = new Node<char>{ 0 };
+		dummy->next = head;
+		Node<char>* prev = dummy;
+		Node<char>* actual = head;
+		int contador = 0;
+		while (actual != nullptr) {
+			contador++;
+			if (contador % k == 0) {
+				prev = reverse(prev, actual->next);
+				actual = prev->next;
+			}
+			else {
+				actual = actual->next;
+			}
+		}
+		return dummy->next;
+	}
 }
--- PAT_Parcial_Reposicion/Ejercicio02.h
+++ PAT_Parcial_Reposicion/Ejercicio02.h
@@ -6,5 +6,6 @@ class Ejercicio02
 {
 public:
 	Node<char>* reverseKGroup(Node<char>* head, int k);
+	Node<char>* reverse(Node<char>* prev, Node<char>* next);
 };
 
