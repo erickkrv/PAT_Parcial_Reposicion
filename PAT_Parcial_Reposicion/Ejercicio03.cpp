fbe8f9a7be00c85211932599ccc889ad959ae9b6
erickkrv <rivaal04@gmail.com>
2023-11-12 11:43:58 -0600

    ejercicio 3

--- PAT_Parcial_Reposicion/Ejercicio03.cpp
+++ PAT_Parcial_Reposicion/Ejercicio03.cpp
@@ -2,5 +2,19 @@
 
 Node<int>* Ejercicio03::detectCycle(Node<int>* head)
 {
+	Node<int>* slow = head;
+	Node<int>* fast = head;
+	while (fast != nullptr && fast->next != nullptr) {
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast) {
+			Node<int>* slow2 = head;
+			while (slow2 != slow) {
+				slow = slow->next;
+				slow2 = slow2->next;
+			}
+			return slow;
+		}
+	}
 	return nullptr;
 }
